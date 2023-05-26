4e3dc87a394b3d33eea51875992138c9be0cbf9b
JJL <jialiang8455@gmail.com>
2023-05-26 14:19:36 +0800

    chore: add the config of database

--- cron/src/constants/configuration.py
+++ cron/src/constants/configuration.py
@@ -19,7 +19,16 @@ class Setting:
 def get_config_setting():
     setting = Setting()
     setting.notify_token = os.getenv("NOTIFY_TOKEN")
-    setting.test_mode = True if os.getenv("TEST_MODE").upper() == "TRUE" else False
-
+    db_host = os.getenv("DB_HOST")
+    db_port = os.getenv("DB_PORT")
+    db_database = os.getenv("DB_DATABASE")
+    db_user = os.getenv("DB_USER")
+    db_password = os.getenv("DB_PASSWORD")
+    setting.config_db = {
+        "host": db_host,
+        "port": int(db_port),
+        "database": db_database,
+        "user": db_user,
+        "password": db_password
+    }
     return setting
-    
