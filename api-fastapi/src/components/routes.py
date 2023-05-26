a14b4a3c7ba8ab00c0e7d6989750183eefefe37f
JJL <jialiang8455@gmail.com>
2023-05-26 21:23:09 +0800

    feat: add FastAPI template

--- api-fastapi/src/components/routes.py
+++ api-fastapi/src/components/routes.py
@@ -1,7 +1,5 @@
 from . import app
 
-@app.get("/test", tags=['志工統計'], summary='取得當季度第九河川局防汛志工清單', description="QQ123")
+@app.get("/test/info", tags=['測試'], summary='測試111', description="description")
 def callback(year: str = "2023"):
-    year = year.replace(" ", "")
     return f"Hello, { year }"
-
--- api-fastapi/src/constants/configuration.py
+++ api-fastapi/src/constants/configuration.py
@@ -15,14 +15,17 @@ class Setting:
             self.props[name] = value
         return 
 
+
 def get_config_setting():
-    setting = Setting()
-    setting.notify_token = os.getenv("NOTIFY_TOKEN")
+
     db_host = os.getenv("DB_HOST")
     db_port = os.getenv("DB_PORT")
     db_database = os.getenv("DB_DATABASE")
     db_user = os.getenv("DB_USER")
     db_password = os.getenv("DB_PASSWORD")
+
+    setting = Setting()
+
     setting.config_db = {
         "host": db_host,
         "port": int(db_port),
@@ -30,5 +33,11 @@ def get_config_setting():
         "user": db_user,
         "password": db_password
     }
+    setting.api_description = {
+        "title": os.getenv("API_TITLE"),
+        "description": os.getenv("API_DESCRIPTION"),
+        "root_path": os.getenv("API_ROOT_PATH"),
+    }
+
     return setting
     
